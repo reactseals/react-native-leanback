78732b503d0dedc821c9a9aacc3e119bd0a93990
aurimasmi <aurimasmickys@gmail.com>
2020-08-27 16:31:09 +0300

    improvement: update glide, remove deprecated method

--- android/src/main/java/com/rs/leanbacknative/LeanbackNativePackage.java
+++ android/src/main/java/com/rs/leanbacknative/LeanbackNativePackage.java
@@ -17,11 +17,6 @@ public class LeanbackNativePackage implements ReactPackage {
     }
 
     @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return null;
-    }
-
-    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Arrays.<ViewManager>asList(
                 new LeanbackNativeRowManager(),
