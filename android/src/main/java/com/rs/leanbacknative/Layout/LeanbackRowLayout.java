71d7674c9b3e16ed42a7d5bd6afe63a2ba911bc4
aurimasmi <aurimasmickys@gmail.com>
2020-08-28 19:53:33 +0300

    fix for navigation between different rows and when onItemSelected is not triggered

--- android/src/main/java/com/rs/leanbacknative/Layout/LeanbackRowLayout.java
+++ android/src/main/java/com/rs/leanbacknative/Layout/LeanbackRowLayout.java
@@ -3,11 +3,15 @@ package com.rs.leanbacknative.Layout;
 import android.annotation.SuppressLint;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
 import android.widget.FrameLayout;
 
 import androidx.annotation.NonNull;
 import androidx.leanback.app.RowsFragment;
 import androidx.leanback.widget.ArrayObjectAdapter;
+import androidx.leanback.widget.BaseOnItemViewSelectedListener;
 import androidx.leanback.widget.HeaderItem;
 import androidx.leanback.widget.ListRow;
 import androidx.leanback.widget.ListRowPresenter;
@@ -37,6 +41,8 @@ public class LeanbackRowLayout extends FrameLayout {
     private ArrayObjectAdapter mRowsAdapter;
 
     private String mRowTitle;
+    private NativeRowItem mLastSelectedItem;
+    private List<NativeRowItem> mRows;
 
 
     public LeanbackRowLayout(@NonNull ThemedReactContext context, RowsFragment rowsFragment) {
@@ -62,16 +68,39 @@ public class LeanbackRowLayout extends FrameLayout {
         rowsFragment.setOnItemViewSelectedListener(new ItemViewSelectedListener());
     }
 
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event) {
+        // it's a workaround for situation when we have multiple rows and navigating up and down between them
+        // in that case onItemSelected is not triggered, therefore we're returning latest focused item in the row
+        // or first one if row was never focused
+        if (event.getAction() == 1) {
+            switch (event.getKeyCode()) {
+                case KeyEvent.KEYCODE_DPAD_UP:
+                case KeyEvent.KEYCODE_DPAD_DOWN:
+                    if (mLastSelectedItem == null) {
+                        mLastSelectedItem = mRows.get(0);
+                    }
+
+                    WritableMap ev = Arguments.createMap();
+                    ev.putString("item", mLastSelectedItem.toJSON());
+                    mContext.getJSModule(RCTEventEmitter.class).receiveEvent(getId(), "onFocus", ev);
+            }
+        }
+
+        return super.dispatchKeyEvent(event);
+    }
+
     private final class ItemViewSelectedListener implements OnItemViewSelectedListener {
         @Override
         public void onItemSelected(
-            Presenter.ViewHolder itemViewHolder,
-            Object item,
-            RowPresenter.ViewHolder rowViewHolder,
-            Row row) {
+                Presenter.ViewHolder itemViewHolder,
+                Object item,
+                RowPresenter.ViewHolder rowViewHolder,
+                Row row) {
 
             if (item instanceof NativeRowItem) {
                 NativeRowItem nativeRowItem = (NativeRowItem) item;
+                mLastSelectedItem = nativeRowItem;
                 WritableMap event = Arguments.createMap();
                 event.putString("item", nativeRowItem.toJSON());
                 mContext.getJSModule(RCTEventEmitter.class).receiveEvent(getId(), "onFocus", event);
@@ -96,7 +125,7 @@ public class LeanbackRowLayout extends FrameLayout {
     public void setDataAndAttributes(ReadableMap dataAndAttributes) {
         ReadableArray data = dataAndAttributes.getArray("data");
 
-        List<NativeRowItem> rows = DataManager.setupData(data);
+        mRows = DataManager.setupData(data);
         ArrayObjectAdapter mListRowAdapterWithData;
         CardPresenter mCardPresenter;
 
@@ -109,8 +138,8 @@ public class LeanbackRowLayout extends FrameLayout {
 
         mListRowAdapterWithData = new ArrayObjectAdapter(mCardPresenter);
 
-        for (int i = 0; i < rows.size(); i++) {
-            mListRowAdapterWithData.add(rows.get(i));
+        for (int i = 0; i < mRows.size(); i++) {
+            mListRowAdapterWithData.add(mRows.get(i));
         }
 
         HeaderItem header = new HeaderItem(0, mRowTitle);
