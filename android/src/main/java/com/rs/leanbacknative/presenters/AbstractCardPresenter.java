4c3494aa0b110a5ddcc0391572c4b63d4537303b
Ričardas Noreika <noricardas@gmail.com>
2021-05-19 15:29:05 +0300

    fix(Log): Remove Log import, which was used for logging values during development and testing

--- android/src/main/java/com/rs/leanbacknative/presenters/AbstractCardPresenter.java
+++ android/src/main/java/com/rs/leanbacknative/presenters/AbstractCardPresenter.java
@@ -2,7 +2,6 @@ package com.rs.leanbacknative.presenters;
 
 import android.content.Context;
 import android.graphics.drawable.Drawable;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
